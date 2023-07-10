514a1fa4eb31cc5bf8166c2ad9c6af113d63cd79
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-09 23:54:37 -0300

    feat(Display): Add variable data and conditional show to secondary

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,44 +1,41 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text } from "react-native";
 
-export function Display() {
-	return (
-		<View style={style.displayContainer}>
-			<View style={style.primaryContainer}>
-				<Text style={style.primaryTextContainer}>
-					13+22+666
-				</Text>
-			</View>
-			<View style={style.secondaryContainer}>
-				<Text style={style.secondaryTextContainer}>
-					= 123
-				</Text>
-			</View>
-		</View>
-	);
+export function Display({ primaryData, secondaryData }) {
+  return (
+    <View style={style.displayContainer}>
+      <View style={style.primaryContainer}>
+        <Text style={style.primaryTextContainer}>{primaryData}</Text>
+      </View>
+      {secondaryData && (
+        <View style={style.secondaryContainer}>
+          <Text style={style.secondaryTextContainer}>= {secondaryData}</Text>
+        </View>
+      )}
+    </View>
+  );
 }
 
 const style = StyleSheet.create({
-	displayContainer: {
-		padding: 5,
-	},
-	primaryContainer: {
-		paddingHorizontal: 10,
-		paddingVertical: 0
-	},
-	primaryTextContainer: {
-		position: 'relative',
-		fontSize: 50,
-		textAlign: 'right',
-		color: '#404040',
-	},
-	secondaryContainer: {
-		paddingHorizontal: 10,
-		paddingBottom: 10,
-
-	},
-	secondaryTextContainer: {
-		fontSize: 30,
-		textAlign: 'right',
-		color: '#525252',
-	}
+  displayContainer: {
+    padding: 5,
+  },
+  primaryContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 0,
+  },
+  primaryTextContainer: {
+    position: "relative",
+    fontSize: 50,
+    textAlign: "right",
+    color: "#404040",
+  },
+  secondaryContainer: {
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+  },
+  secondaryTextContainer: {
+    fontSize: 30,
+    textAlign: "right",
+    color: "#525252",
+  },
 });
