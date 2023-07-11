ee0cbb1c59d6205f022eb7cb382e0579a06d4783
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-10 23:12:44 -0300

    feat(App): Add logic functions

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import { View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
-import { KeyBoard } from './src/components/KeyBoard';
-import { Display } from './src/components/Display';
+import { KeyBoard } from "./src/components/KeyBoard";
+import { Display } from "./src/components/Display";
+
+import { core } from "./src/utils/core";
 
 export default function App() {
+  const [calcData, setCalcData] = useState("");
+  const [result, setResult] = useState("");
+
+  function calculate() {
+    const result = core(calcData);
+    setCalcData(String(result));
+    setResult("");
+  }
+
+  useEffect(() => {
+    const result = core(calcData);
+    if (result) {
+      setResult(result);
+    } else if (calcData == "") {
+      setResult("");
+    }
+  }, [calcData]);
+
   return (
-    <View style={styles.container}>
-      <Display />
-      <KeyBoard />
+    <View style={[styles.container, { backgroundColor: themeBg }]}>
+      <Display primaryData={calcData} secondaryData={result} />
+      <KeyBoard state={{ calcData, setCalcData }} calculate={calculate} />
     </View>
   );
 }
@@ -16,9 +37,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
     padding: 8,
   },
 });
