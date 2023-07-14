1c9dd41c581953d8c2e952eb4760210698a67be0
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-13 23:59:07 -0300

    feat: Update store

--- src/reducer/store.js
+++ src/reducer/store.js
@@ -1,23 +1,7 @@
-import { createContext, useContext } from "react";
+import { createContext } from "react";
 
 export const AppContext = createContext();
 export const initialState = {
   expression: "",
-};
-
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case "clear":
-        console.log('foi o clear');
-      break;
-    case "bksp":
-      break;
-    case "percent":
-      break;
-    case "equal":
-        console.log('foi o iqual');
-      break;
-    default:
-      return state;
-  }
+  result: ""
 };
