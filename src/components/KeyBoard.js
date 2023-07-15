b66b22e50df36ad7666375f98f1576572f1dac15
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-14 23:21:43 -0300

    Refactory(KeyBoard): Remove comments

--- src/components/KeyBoard.js
+++ src/components/KeyBoard.js
@@ -1,135 +1,47 @@
+import { memo } from "react";
 import { View, StyleSheet } from "react-native";
 
 import Key from "./Key";
 
 function KeyBoard() {
 
-
   return (
     <View>
       <View style={style.keyRow}>
-        <Key
-          innerText="<"
-          style="operation"
-          keyDigit="bksp"
-          key="bksp"
-        />
-        <Key
-          innerText="C"
-          style="operation"
-          keyDigit="clear"
-          key="clear"
-        />
-        <Key
-          innerText="%"
-          style="operation"
-          keyDigit="percent"
-          key="percent"
-        />
-        <Key
-          innerText="/"
-          style="operation"
-          keyDigit="/"
-          key="/"
-        />
+        <Key innerText="<" style="operation" keyDigit="bksp" key="bksp" />
+        <Key innerText="C" style="operation" keyDigit="clear" key="clear" />
+        <Key innerText="%" style="operation" keyDigit="percent" key="percent" />
+        <Key innerText="/" style="operation" keyDigit="/" key="/" />
       </View>
       <View style={style.keyRow}>
-        <Key
-          innerText="1"
-          keyDigit="1"
-          key="1"
-        />
-        <Key
-          innerText="2"
-          keyDigit="2"
-          key="2"
-        />
-        <Key
-          innerText="3"
-          keyDigit="3"
-          key="3"
-        />
-        <Key
-          innerText="*"
-          style="operation"
-          keyDigit="*"
-          key="*"
-        />
+        <Key innerText="1" keyDigit="1" key="1" />
+        <Key innerText="2" keyDigit="2" key="2" />
+        <Key innerText="3" keyDigit="3" key="3" />
+        <Key innerText="*" style="operation" keyDigit="*" key="*" />
       </View>
       <View style={style.keyRow}>
-        <Key
-          innerText="4"
-          keyDigit="4"
-          key="4"
-        />
-        <Key
-          innerText="5"
-          keyDigit="5"
-          key="5"
-        />
-        <Key
-          innerText="6"
-          keyDigit="6"
-          key="6"
-        />
-        <Key
-          innerText="-"
-          style="operation"
-          keyDigit="-"
-          key="-"
-        />
+        <Key innerText="4" keyDigit="4" key="4" />
+        <Key innerText="5" keyDigit="5" key="5" />
+        <Key innerText="6" keyDigit="6" key="6" />
+        <Key innerText="-" style="operation" keyDigit="-" key="-" />
       </View>
       <View style={style.keyRow}>
-        <Key
-          innerText="7"
-          keyDigit="7"
-          key="7"
-        />
-        <Key
-          innerText="8"
-          keyDigit="8"
-          key="8"
-        />
-        <Key
-          innerText="9"
-          keyDigit="9"
-          key="9"
-        />
-        <Key
-          innerText="+"
-          style="operation"
-          keyDigit="+"
-          key="+"
-        />
+        <Key innerText="7" keyDigit="7" key="7" />
+        <Key innerText="8" keyDigit="8" key="8" />
+        <Key innerText="9" keyDigit="9" key="9" />
+        <Key innerText="+" style="operation" keyDigit="+" key="+" />
       </View>
       <View style={style.keyRow}>
-        <Key
-          innerText="."
-          keyDigit="."
-          key="."
-        />
-        <Key
-          innerText="0"
-          keyDigit="0"
-          key="0"
-        />
-        <Key
-          innerText=","
-          keyDigit=","
-          key=","
-        />
-        <Key
-          innerText={"="}
-          style="equal"
-          keyDigit="equal"
-          key="equal"
-        />
+        <Key innerText="." keyDigit="." key="." />
+        <Key innerText="0" keyDigit="0" key="0" />
+        <Key innerText="," keyDigit="," key="," />
+        <Key innerText={"="} style="equal" keyDigit="equal" key="equal" />
       </View>
     </View>
   );
 }
 
-export default KeyBoard;
+export default memo(KeyBoard)
 
 const style = StyleSheet.create({
   keyRow: {
